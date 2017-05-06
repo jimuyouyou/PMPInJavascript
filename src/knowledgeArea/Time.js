6633c662cf4060fc868a012ec27062203004de15
jimuyouyou <qinfuxian2014@outlook.com>
2017-05-06 17:04:56 +0800

    time init

---
+++ src/knowledgeArea/Time.js
@@ -0,0 +1,11 @@
+'use strict'
+class Time {
+	planScheduleManagement(){}
+	defineActivities(){}
+	sequenceActivities(){}
+	estimateActivityResources(){}
+	esitimateActivityDurations(){}
+	developSchedule(){}
+	controlSchedule(){}
+}
+module.exports = Time;
